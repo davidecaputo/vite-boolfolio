79db95c4fd4993e7e98997b28f1f073f3223d39e
davidecaputo <daca9621@gmail.com>
2023-06-14 01:15:34 +0200

    add getData

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <h1 class="text-center">Lavori</h1>
+  <h1 class="text-center">{{ title }}</h1>
 </template>
 
 <script>
-  export default {
-    
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      title: 'Lavori',
+      works: [],
+      apiUrl: 'http://127.0.0.1:8000/api'
+    }
+  },
+  methods: {
+    getData(){
+      axios.get(`${this.apiUrl}/works`).then((el) => {
+        console.log(el.data.results.data);
+        this.works = el.data.results.data;
+      })
+    }
+  },
+  mounted() {
+    this.getData();
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
