fa0d39a7c7a2fcc71a33d36a789a7797f2bcd1ce
davidecaputo <daca9621@gmail.com>
2023-06-19 15:13:12 +0200

    add show

--- src/pages/AllWorks.vue
+++ src/pages/AllWorks.vue
@@ -1,15 +1,16 @@
 <template>
   <main>
     <div class="container">
-      <h1 class="text-center m-5">{{ title }}</h1>
+      <h1 class="text-center m-4">{{ title }}</h1>
       <div class="row">
         <div class="col" v-for="work in works" :key="work.id">
-          <div class="card h-100">
-            <img :src="work.image" :alt="work.name" class="card-img-top">
+          <div class="card h-100 rounded-5">
+            <img :src="work.image" :alt="work.name" class="card-img-top p-1 rounded-5 bg-success">
             <div class="card-body">
               <h4>{{ work.name }}</h4>
               <p>{{ work.description }}</p>
               <p v-if="work.type">{{ work.type.name }}</p>
+              <router-link :to="{ name: 'single-work', params: { slug: work.slug } }" class="btn btn-primary">Dettagli</router-link>
             </div>
           </div>
         </div>
--- src/pages/SingleWork.vue
+++ src/pages/SingleWork.vue
@@ -1,12 +1,43 @@
 <template>
-    <div>
+    <main>
+        <div class="container d-flex flex-column align-items-center gap-2 py-3">
+            <h1 class="text-center">Prodotto</h1>
+            <div class="card w-50 rounded-5" v-if="work">
+                <img :src="work.image" :alt="work.name" class="p-2 rounded-5 bg-success">
+                <div class="card-body">
+                    <p>{{ work.name }}</p>
+                    <p>{{ work.description }}</p>
+                    <p v-if="work.type">{{ work.type.name }}</p>
+                    <p v-for="language in work.languages">{{ language.name }}</p>
+                </div>
+            </div>
+        </div>
 
-    </div>
+    </main>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
-        
+        data() {
+            return {
+                work: null,
+                apiUrl: 'http://127.0.0.1:8000/api'
+            }
+        },
+        methods: {
+            getData(){
+                axios.get(`${this.apiUrl}/works/${this.$route.params.slug}`).then((el) => {
+                    console.log(el);
+                    this.work = el.data.results;
+                });
+            }
+        },
+        mounted () {
+            this.getData();
+            // console.log(this.$route);
+            // console.log(this.$router);
+        },
     }
 </script>
 
